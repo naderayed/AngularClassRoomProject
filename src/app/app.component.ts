953896b77615a64a1aed2e7ec113881d1cdb00c7
NaderAyed <nader.ayed@esprit.tn>
2022-09-27 10:15:37 +0100

    adding html content and install bootstrap , poppy..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
 
-@Component({
+@Component({//meta data 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
+@NgModule({ // meta data
+  declarations: [ // list of component 
+    AppComponent,
+    TestComponent
   ],
-  imports: [
+  imports: [// 
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],// services
+  bootstrap: [AppComponent] // first component runnig up ( at starting)
 })
 export class AppModule { }
